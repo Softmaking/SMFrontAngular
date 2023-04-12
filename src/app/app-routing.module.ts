f91cb9b2861f562a7f582bc2bdcfed17a77e811b
Israel Jasma <israel.jasma@gmail.com>
2023-04-11 22:03:57 -0400

    Feat: routing auth login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomeModule)
   },
   {
+    path:'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule)
+  },
+  {
     path: '**',
     redirectTo: '',
     pathMatch: 'full'
